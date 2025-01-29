17fa08eecdefe6064e4b706f0a5edd38ecc015bd
Ksayus <2481374335@qq.com>
2025-01-30 00:38:58 +0800

    updated20250130_xk

--- git.sh
+++ git.sh
@@ -1,6 +1,9 @@
-git init
+#!/bin/bash
+# 获取当前日期，格式为 YYYYMMDD
+current_date=$(date +%Y%m%d)
+
 git add .
-git commit -m "first commit"
+git commit -m "updated${current_date}_xk"
 git remote set-url origin https://github.com/ksayus/PCSMT-2.git
 git branch -M main
 git push -u origin main -f
