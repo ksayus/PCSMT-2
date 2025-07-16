6fb93d8d3d365d333c75f161cd49196f6bf2485c
Ksayus <108785703+ksayus@users.noreply.github.com>
2025-07-16 16:31:32 +0800

    Delete bin directory

--- bin/api/PageProgram.py
+++
@@ -1,6 +0,0 @@
-import bin.api.main as main
-from flask import render_template
-
-@main.app.route('/program/version', endpoint="program_version")
-def program_version():
-    return render_template('ProgramVersion.html')
--- bin/export/numbers.py
+++
@@ -1,5 +0,0 @@
-def swap_numbers(number1, number2):
-    """
-    交换两个数字的数值
-    """
-    return number2, number1
